99d7ed9b6020181a6ddca4c9bf18a829981853b8
SerhiiKutovyi <wagenrex@gmail.com>
2022-10-13 22:43:54 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -10,10 +10,11 @@ console.log(refGallery);
 
 const newItem = galleryItems
   .map(
-    item => `
-  <a class="gallery__item" href = "${item.original}">
-  <img class="gallery__image" src="${item.preview}"  alt = "${item.description}"/>
-  </a>`
+    {
+      original,
+      preview,
+      description,
+    }`<a class="gallery__item" href = "${original}"><img class="gallery__image" src="${preview}"  alt = "${description}"/></a>`
   )
   .join('');
 
